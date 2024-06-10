17bdf7ee1b0d2d381a8a8055eb707569822486f1
Jhoan Perez <jhoanjesus3@gmail.com>
2024-06-10 14:41:39 -0500

    repositorio de php

---
+++ php/conexion_be.php
@@ -0,0 +1,9 @@
+<?php
+    $conexion = mysqli_connect("localhost", "root", "", "gestion_usuarios");
+
+    /*if($conexion){
+        echo 'Conexión exitosa';
+    }else{
+        echo 'Conexión fallida';
+    }*/
+?>
